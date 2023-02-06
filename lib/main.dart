99d3845d45f2b5a014a46601eff02b9115250e9a
myrin <myring100@gmail.com>
2023-02-06 09:21:43 -0800

    asdasd

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,83 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
+
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:home_widget/home_widget.dart';
+import 'package:scroll_date_picker/scroll_date_picker.dart';
 import 'package:that_day/screen/firstPage.dart';
 import 'package:that_day/service/notificationHelper.dart';
+import 'package:workmanager/workmanager.dart';
+
+@pragma("vm:entry-point")
+void callbackDispatcher() {
+  Workmanager().executeTask((taskName, inputData) {
+    final now = DateTime.now();
+    return Future.wait<bool?>([
+      HomeWidget.saveWidgetData(
+        'title',
+        'Updated from Background',
+      ),
+      HomeWidget.saveWidgetData(
+        'message',
+        'Updated from Background',
+      ),
+      HomeWidget.updateWidget(
+        name: 'HomeWidgetExampleProvider',
+        iOSName: 'HomeWidgetExample',
+      ),
+    ]).then((value) {
+      return !value.contains(false);
+    });
+  });
+}
+
+/// Called when Doing Background Work initiated from Widget
+@pragma("vm:entry-point")
+void backgroundCallback(Uri? data) async {
+  print(data);
+
+  if (data?.host == 'titleclicked') {
+    final greetings = [
+      'Hello',
+      'Hallo',
+      'Bonjour',
+      'Hola',
+      'Ciao',
+      '哈洛',
+      '안녕하세요',
+      'xin chào'
+    ];
+    final selectedGreeting = greetings[Random().nextInt(greetings.length)];
+
+    await HomeWidget.saveWidgetData<String>('title', selectedGreeting);
+    await HomeWidget.saveWidgetData<String>('message', selectedGreeting);
+    await HomeWidget.updateWidget(
+        name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
+  }
+}
 
 void main() {
   NotificationHelper.notificationInit();
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: kDebugMode);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
   static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -24,7 +90,7 @@ class MyApp extends StatelessWidget {
         print('ontap down()in main page');
       },
       child: GetMaterialApp(
-        navigatorKey: navigatorKey,
+        navigatorKey: MyApp.navigatorKey,
         title: 'That Day(D-Day)',
         theme: ThemeData(
           appBarTheme:  AppBarTheme(color: Colors.lightBlue[500])
@@ -33,5 +99,92 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    HomeWidget.setAppGroupId('YOUR_GROUP_ID');
+    HomeWidget.registerBackgroundCallback(backgroundCallback);
+  }
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _checkForWidgetLaunch();
+    HomeWidget.widgetClicked.listen(_launchedFromWidget);
+  }
+
+  Future<Future<List<bool?>>> _sendData() async {
+    try {
+      return Future.wait([
+        HomeWidget.saveWidgetData<String>('title', _titleController.text),
+        HomeWidget.saveWidgetData<String>('message', _messageController.text),
+      ]);
+    } on PlatformException catch (exception) {
+      debugPrint('Error Sending Data. $exception');
+    }
+    throw{};
+  }
+
+  Future<Future<bool?>> _updateWidget() async {
+    try {
+      return HomeWidget.updateWidget(
+          name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
+    } on PlatformException catch (exception) {
+      debugPrint('Error Updating Widget. $exception');
+    }
+    throw{};
+  }
+
+  Future<Future<List<String?>>> _loadData() async {
+    try {
+      return Future.wait([
+        HomeWidget.getWidgetData<String>('title', defaultValue: 'Default Title')
+            .then((value) => _titleController.text = value!),
+        HomeWidget.getWidgetData<String>('message',
+            defaultValue: 'Default Message')
+            .then((value) => _messageController.text = value!),
+      ]);
+    } on PlatformException catch (exception) {
+      debugPrint('Error Getting Data. $exception');
+    }
+    throw{};
+  }
+
+  Future<void> _sendAndUpdate() async {
+    await _sendData();
+    await _updateWidget();
+  }
+
+  void _checkForWidgetLaunch() {
+    HomeWidget.initiallyLaunchedFromHomeWidget().then(_launchedFromWidget);
+  }
+
+  void _launchedFromWidget(Uri? uri) {
+    if (uri != null) {
+      showDialog(
+          context: context,
+          builder: (buildContext) => AlertDialog(
+            title: Text('App started from HomeScreenWidget'),
+            content: Text('Here is the URI: $uri'),
+          ));
+    }
+  }
+
+  void _startBackgroundUpdate() {
+    Workmanager().registerPeriodicTask('1', 'widgetBackgroundUpdate',
+        frequency: Duration(minutes: 15));
+  }
+
+  void _stopBackgroundUpdate() {
+    Workmanager().cancelByUniqueName('1');
+  }
+
 }
+
 
